ac9b36c29fd63a45b6849fe239ec7a0130f1f1dc
driptard00 <akintadeseun816@gmail.com>
2022-11-22 07:48:32 -0800

    Routes to build

--- routes/blog.js
+++ routes/blog.js
@@ -5,7 +5,24 @@ const {
     SampleCode
 } = require("../controllers/blog")
 
-//TODO Protect Router 
 router.get("/test", SampleCode);
+//TODO  Router Implement 
+// router.get("/posts", SampleCode);
+// router.get("/post/:id", SampleCode);
+
+// router.get("/post/manage", SampleCode);
+// router.post("/post/create/:id", SampleCode);
+// router.delete("/post/delete/:id", SampleCode);
+// router.edit("/post/edit/:id", SampleCode);
+
+// router.get("/comment/:postid", SampleCode);
+// router.put("/comment/create/:postid", SampleCode);
+// router.put("/comment/edit/:id", SampleCode); // Comment can only be deleted so might be depercated  
+// router.delete("/comment/delete/:id", SampleCode);
+// router.post("/comment/reply/:id", SampleCode);
+
+// router.post("/leading/create/", SampleCode);
+// router.put("/leading/edit/", SampleCode);
+
 
 module.exports = router;
